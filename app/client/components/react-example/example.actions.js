09447f47edbb35cb870e310b678a2eaf4d6aaedf
itechdom <osamah.net.m@gmail.com>
2016-05-19 13:45:28 -0600

    commented on the use of actions

--- app/client/components/react-example/example.actions.js
+++ app/client/components/react-example/example.actions.js
@@ -3,6 +3,8 @@ var Rx = require('rx');
 var dispatcher = require('../../lib/dispatcher/dispatcher.js');
 var clientActions = require('../client/client.actions.js')
 
+//actions we want to export to other components, for example, we want to know if the whole layout has shifted to something else
+//basically, if just provides an interface for any event happening within this component
 class actionMain{
   constructor(){
     var request = clientActions['changeRoute$'];
--- app/client/components/react-example/example.js
+++ app/client/components/react-example/example.js
@@ -4,17 +4,11 @@ var model = require('./example.model.js');
 var dispatcher = require('../../lib/dispatcher/dispatcher.js');
 
 class exampleMain{
-
 	constructor(){
-
-		this.view = view;
-
         //load data and dispatch the event
         var example = model.getExample();
         dispatcher.emit("example.dataLoaded$",example);
-
-
 	}
-
 }
+
 module.exports = new exampleMain();
